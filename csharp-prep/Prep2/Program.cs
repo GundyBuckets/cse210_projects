17179aa4ad32142dd48385ec04c6b55f819be715
GundyBuckets <145598616+GundyBuckets@users.noreply.github.com>
2024-09-17 15:08:11 -0600

    C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your letter grade is A");
+        } 
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your letter grade is D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("Your letter grade is F");
+        }
     }
 }
