2f175088cf1df89597c9a54a4ddd22ab89b11354
GundyBuckets <145598616+GundyBuckets@users.noreply.github.com>
2024-10-16 12:55:28 -0600

    Encapsulation Learning (part 1)

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // First fraction is 1/1
+        Fraction firstNum = new();
+        Console.WriteLine(firstNum.GetFractionString());
+        Console.WriteLine(firstNum.GetDecimalValue());
+
+        // Second fraction is 5/1
+        Fraction secondNum = new Fraction(5);
+        Console.WriteLine(secondNum.GetFractionString());
+        Console.WriteLine(secondNum.GetDecimalValue());
+
+        // Third fraction is 3/4
+        Fraction thirdNum = new Fraction(3, 4);
+        Console.WriteLine(thirdNum.GetFractionString());
+        Console.WriteLine(thirdNum.GetDecimalValue());
+
+        // Fourth fraction is 1/3
+        Fraction fourthNum = new Fraction(1, 3);
+        Console.WriteLine(fourthNum.GetFractionString());
+        Console.WriteLine(fourthNum.GetDecimalValue());
     }
 }
